096a8646e4c2d7d7bd32c95fe9a2c771c6d28474
rakkin493 <rakkin493@cosml-1687611.hsd1.co.comcast.net>
2019-08-31 16:30:21 -0600

    Introducing lombok

--- spring-boot-mysql-jpa-hibernate-swagger-master_spring-boot-mysql-jpa-hibernate-swagger-master/src/main/java/com/springboot/domain/User.java
+++ spring-boot-mysql-jpa-hibernate-swagger-master_spring-boot-mysql-jpa-hibernate-swagger-master/src/main/java/com/springboot/domain/User.java
@@ -5,9 +5,16 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * Represents an User for this web application.
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 // @Table(name = "users")
 public class User {
@@ -15,45 +22,15 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-
     private String email;
-
     private String name;
-
-    public User() {
-    }
 
     public User(long id) {
         this.id = id;
     }
-
     public User(String email, String name) {
         this.email = email;
         this.name = name;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long value) {
-        this.id = value;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String value) {
-        this.email = value;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String value) {
-        this.name = value;
     }
 
 } // class User
